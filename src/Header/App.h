448463ee0bbbc3c4ffa7729a10c1ae39b2bd679a
Joy Ray <joys57725@gmail.com>
2026-06-20 11:16:59 +0600

    Added functions

---
+++ src/Header/App.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Log.h"
+
+#include <string>
+#include <fstream>
+
+class App{
+
+    private:
+
+        Log log;
+
+        std::ofstream write_json_file;
+        std::ifstream read_json_file;
+
+        std::string json_file_name = "app_commands.json";
+        std::string appmanager_folder = "appmanager/";
+        std::string home_dir;
+
+        std::string info = "[ℹ] ";
+        std::string success = "[✔] ";
+        std::string error = "[✖] ";
+
+        void output(const std::string& id, const std::string& title);
+        void print(const std::string& message);
+        void help_print(const std::string& cmd_name, const std::string& description);
+        void empty_app_info();
+
+
+    public:
+        App();
+        ~App();
+
+        void home();
+        void add_command();
+        void run_command(const std::string& id);
+        void delete_command();
+        void reset_command();
+        void log_clear(const std::string& text);
+        void list();
+        void version(const std::string& version);
+        void help();
+
+};
